2aeca940c0e0e7de6a1a886c886527730826d1bf
Nic Bonetto <nick.bonetto@gmail.com>
2017-06-13 14:46:59 -0700

    Created array for songs. #1

---
+++ main.js
@@ -0,0 +1,14 @@
+var songs = [
+  {
+    artist: 'Ben Howard'
+    title: 'I Forget Where We Were'
+  },
+  {
+    artist: 'Red Hot Chili Peppers'
+    title: 'Snow (Hey Oh)'
+  },
+  {
+    artist: 'Calvin Harris feat. Frank Ocean'
+    title: 'Slide'
+  }
+]
